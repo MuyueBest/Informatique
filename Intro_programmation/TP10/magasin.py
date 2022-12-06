371871a8285c6eeb82e6b82adbea4fd823e569c0
Muyue 02 <102559201+MuyueBest@users.noreply.github.com>
2022-12-06 14:09:16 +0000

    06/12/2022 15:00

--- Intro_programmation/TP10/magasin.py
+++ Intro_programmation/TP10/magasin.py
@@ -2,41 +2,67 @@
 d_pu = {"Épée en mousse":5, "Masque Dark Vador":30, "Hand spinner 3d":10, "Console Minux":150,"Lego Footix":15}
 d_stock = {"Épée en mousse":10, "Masque Dark Vador":4, "Hand spinner 3d":10, "Console Minux":2,"Lego Footix":5}
 
+for cle, valeur in d_stocks.items():
+	if valeur <= 2:
+		print(f"il faut remplir le stock de {cle}")
+
 #exo 2
-def verification(d1:dict, d2:dict)->bool:
-	"""Vérifie que deux dictionnaires ont exactement les mêmes clés."""
-	cle_d1 = [cle for cle in d1.keys()]
-	cle_d2 = [cle for cle in d2.keys()]
-	
-	for j in range(len(cle_d1)):
-		if cle_d1[j] != cle_d2[j]:
-				return False
-	return True
-	
+def verification(d1, d2):
+	for i in d1.keys():
+		if i not in d2:
+			return False
+	for j in d2.keys():
+        if j not in d1:
+            return False
+    return True
+
+
 assert verification(d_pu, d_stock) == True
+del d_pu["Lego Footix"]
+assert verification(d_pu,  d_stock) == False
 
 #exo 3
-def achat_possible(k:str, v:int, d:dict):
-	"""Indique si l’on peut acheter le produit k en quantité v """
-	if d[k] >= v:
-		return True
-	return False
+def achat_possible(k ,v, d):
+	""" truc """
+      if k in d:
+            return d[k] >= v
+      return False
 
 assert achat_possible("Hand spinner 3d", 2, d_stock) == True
 assert achat_possible("Console Minux", 5, d_stock) == False
+assert achat_possible(1 , 1, {}) == False
+assert achat_possible("Hand spinner 3d" , 2, d_stock) == True
 
 #exo 4
-def achats_possibles(k, v, d=None):
-	"""Indique si l’on peut acheter le produit k en quantité v """
-	if d != None:
-		return achat_possible(k, v, d)
-	else:
-		liste_tuple = [tuple for tuple in k.items()]
-		for tuple in liste_tuple:
-			if tuple[1] > v[tuple[0]]:
-				return False
-		return True
+def achats_possibles(d_achats, d):
+	""" truc """
+    for k, v in d_achats.items():
+        r = achat_possible(k ,v, d)
+        if not r:
+            return r
+    return True
 
+assert achats_possibles({"Hand spinner 3d": 2, "Console Minux": 5}, d_stock) == False
 assert achats_possibles({"Hand spinner 3d": 2}, d_stock) == True
 d_achats = {"Hand spinner 3d": 2, "Console Minux": 5}
 assert achats_possibles(d_achats, d_stock) == False
+
+#exo4(bonus)
+
+def achats(d_achats, d_pu, d_stocks, facture=True):
+	""" truc """
+    total = 0
+    for k, v in d_achats.items():
+        r = achat_possible(k ,v, d_stocks)
+        if r:
+            d_stocks[k] = d_stocks[k] - v
+            if facture:
+                total = total + v * d_pu[k]
+                print(f"{k}: {v} x {d_pu[k]} = {v * d_pu[k]}")
+    if facture:
+        print("-"*20)
+        print(f"Prix total: {total}")
+
+
+achats({"Hand spinner 3d": 2, "Lego Footix": 1}, d_pu, d_stock, True)
+assert d_stock == {'Épée en mousse': 10, 'Masque Dark Vador': 4, 'Hand spinner 3d': 8, 'Console Minux': 2, 'Lego Footix': 4}
